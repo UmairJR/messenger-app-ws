7e691d56316b5427d70da73c298543dc320844da
UmairJR <umairjr1265@gmail.com>
2024-12-12 18:53:56 +0530

    Fixing issue for deployment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const [time, setTime] = useState(new Date().toLocaleString());
