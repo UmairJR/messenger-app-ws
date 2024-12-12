e8a197f857a8f2aec69953c43d07d180ab28f963
UmairJR <umairjr1265@gmail.com>
2024-12-12 19:54:13 +0530

    fixing footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <footer className=" flex justify-center absolute bottom-0 w-full bg-gradient-to-r from-indigo-600/60 via-blue-500/70 to-indigo-400/70 text-white px-2 py-1">
+    <footer className=" flex justify-center fixed bottom-0 w-full bg-gradient-to-r from-indigo-600/60 via-blue-500/70 to-indigo-400/70 text-white px-2 py-1">
       <div className="flex items-center justify-between">
         <h1 className="text-md sm:text-lg font-bold">
           Copyright © 2024 | <span>Umair JR</span> 
